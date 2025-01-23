cfd87e93798b4fac5f76001fa45466491a8f4bb2
Bo <58004232+Aswe1@users.noreply.github.com>
2025-01-23 19:40:15 +0200

    Project start

---
+++ src/main/java/org/example/discordripoff/DiscordRipoffApplication.java
@@ -0,0 +1,13 @@
+package org.example.discordripoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordRipoffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordRipoffApplication.class, args);
+    }
+
+}
